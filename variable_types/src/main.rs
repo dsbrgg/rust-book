64951200a91a4eaa7db7e9fb1af6d1b871ce7d57
Diego Braga <dsbrgg@gmail.com>
2018-11-29 20:55:11 +0000

    studying more variable types

--- variable_types/src/main.rs
+++ variable_types/src/main.rs
@@ -35,4 +35,71 @@ fn main() {
     println!("Integer 32: {}", integer32);
     println!("Isize: {}", integer_arch);
     println!("bin_u8: {}", bin_u8);
+
+    // floating points
+    // Rust has two primitive floating points
+    // f32  && f64
+    // default type is f64 since it's roughly
+    // the same speed as f32 but with more precision
+    
+    let _x_f64 = 2.0;  // -> f64
+    let _x_f32: f32 = 3.0;  // -> f32
+
+    // boolean
+    // booleans have one byte in size
+
+    let _is_true = true;
+    let _is_false: bool = false;
+
+    // char type
+    // char is the most primitive alphabetic type in Rust
+    // character literals denoted with single quotes (eg. '')
+    // string literals arev denoted with double quotes (eg. "")
+    // it represents a unicode scalar value that ranges from
+    // U+0000 to U+D7FF and U+E000 to U+10FFFF inclusive
+
+    let _c = 'z';
+    let _z = 'ℤ';
+    let _heart_eyed_cat = '😻';
+
+    println!("From unicode: \u{00DF}");
+
+    // tuples
+    // tuples are a way of grouping together values
+    // with different types into one compound type
+    // once declared, they can't grow or shrink in size
+
+    // declaring instantly
+    let _tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // accessing tuple through index
+
+    let _index = _tup.2;
+
+    println!("Last tuple element is: {}", _index);
+
+    // destructuring tuple
+    let tup2 = (400, 6.9, 2);
+
+    let (_x, _y, _z) = tup2;
+
+    println!("The value of y is: {}", _y);
+
+    // arrays
+    // arrays in Rust must have elements of the same type
+    // like tuples, they must also are of fixed length
+    // arrays are good when you want your information
+    // allocated on the stack rather than on the heap
+    // or when you want to ensure you always 
+    // have a fixed number of elements
+
+    let _array = [ 1, 2, 3, 4, 5 ];
+
+    // declaring with [ type, length ]
+    let _array_2: [ i32; 5 ] = [ 6, 7, 8, 9, 10 ];
+
+    // When accessing index out of bounds in the array
+    // Rust will panick and throw an error because of
+    // its safety principles. Other low level
+    // programming languages will allow to access invalid memory
 }
