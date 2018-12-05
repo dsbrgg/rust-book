ce920d6a5ae1398f4f84db1d5279cc658694b729
Diego Braga <dsbrgg@gmail.com>
2018-12-05 09:35:59 +0000

    added reference slices

--- slice-type/README.md
+++ slice-type/README.md
@@ -23,3 +23,34 @@ let s = String::from("hello world");
 let hello = &s[0..=4];
 let world = &s[6..=10];
 ```
+
+If you want to start at the beginning or the end of the string implicitly:
+
+```rust
+let s = String::from("hello");
+
+let slice = &s[..2];
+let slice = &s[2..];
+```
+
+If you want to slice the entire string:
+
+```rust
+let s = String::from("hello");
+
+let len = s.len();
+
+let slice = &s[..];
+let slice = &s[0..len];
+```
+
+> Note: String slice range indices must occur at valid UTF-8 character boundaries. If you attempt to create a string slice in the middle of a multibyte character, your program will exit with an error. For the purposes of introducing string slices, we are assuming ASCII only in this section.
+
+## String Literals are Slices
+
+```rust
+let s = "Hello, world!";
+```
+
+The type `s` here is a `&str`: it's a slice pointing to a specific point of the binary. This is
+why string literals are immutable as well; `&str` is an immutable reference.
--- slice-type/src/main.rs
+++ slice-type/src/main.rs
@@ -2,15 +2,17 @@ fn main() {
   let mut s = String::from("hello world");
   let word = first_word(&s); // word will get the value 5
 
-  s.clear(); // This empties the String, making it equal to ""
+  // s.clear(); // This empties the String, making it equal to "" 
+  // it will throw an error when first_word implements the return value of a str reference
+  // this happens because s.clear() tries to take a mutable reference, which is not the case
 
   println!("string length or first word index: {}", word);
   // world still has the value 5 here, but there's no more string that
   // we could meaningfully use the value 5 with. word is now totally invalid!
 }
 
-fn first_word(s: &String) -> usize {
-  //Because we need to go through the String element by element and check whether a value is a space,
+fn first_word(s: &String) -> &str {
+  // Because we need to go through the String element by element and check whether a value is a space,
   // we’ll convert our String to an array of bytes using the as_bytes method:
   let bytes = s.as_bytes();
 
@@ -21,9 +23,10 @@ fn first_word(s: &String) -> usize {
   // This is a bit more convenient than calculating the index ourselves.
   for (i, &item) in bytes.iter().enumerate() {
     if item == b' ' {
-      return i;
+      // return i; // this will return the position, which
+      return &s[0..i];
     }
   }
 
-  s.len()
+  &s[..]
 }
