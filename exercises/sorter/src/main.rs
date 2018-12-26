7aa2bbea28804ab49d168ec8a29b235279e40249
Diego Braga <dsbrgg@gmail.com>
2018-12-26 20:18:27 +0000

    sorter exercise completed

--- exercises/sorter/src/main.rs
+++ exercises/sorter/src/main.rs
@@ -1,15 +1,21 @@
 fn main() {
-  order("is2 Thi1s T4est 3a");
+  returns_expected();
 }
 
-fn order(string: &str) {
-  let mut s: Vec<&str> = string.split(' ').collect();
-  s.sort_by(|a, b| a.find(char::is_numeric).cmp(&b.find(char::is_numeric)).reverse());
+fn order(string: &str) -> String {
+  let mut s: Vec<_> = string.split(' ').collect();
 
-  println!("{:#?}", s.join("\u{0020}"));
+  s.sort_by(|a, b| {
+    let cc: Vec<&str> = a.matches(char::is_numeric).collect();
+    let nc: Vec<&str> = b.matches(char::is_numeric).collect();
+    
+    cc[0].cmp(nc[0])
+  });
+
+  String::from(s.join("\u{0020}"))
 }
 
-// fn returns_expected() {
-//   assert_eq!(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est");
-//   assert_eq!(order(""), "");
-// }
+fn returns_expected() {
+  assert_eq!(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est");
+  assert_eq!(order(""), "");
+}
