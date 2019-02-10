13672fa7a7410ebd0fc4d5065f27ed21aebe1ebf
Diego Braga <dsbrgg@gmail.com>
2019-02-10 13:58:14 +0000

    refactor: use hash maps on closure example

--- 16_functional_features/src/main.rs
+++ 16_functional_features/src/main.rs
@@ -1,29 +1,37 @@
+// standard crates
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
-struct Cacher<T>
-  where T: Fn(u32) -> u32
+// Traits
+use std::hash::Hash;
+use std::clone::Clone;
+
+struct Cacher<T, A>
+  where T: Fn(A) -> u32,
+        A: Eq + Hash + Clone
 {
   calculation: T,
-  value: Option<u32>
+  value: HashMap<A, u32>
 }
 
-impl<T> Cacher<T>
-  where T: Fn(u32) -> u32
+impl<T, A> Cacher<T, A>
+  where T: Fn(A) -> u32,
+        A: Eq + Hash + Clone
 {
-  fn new(calculation: T) -> Cacher<T> {
+  fn new(calculation: T) -> Cacher<T, A> {
     Cacher {
       calculation,
-      value: None
+      value: HashMap::new()
     }
   }
 
-  fn value(&mut self, arg: u32) -> u32 {
-    match self.value {
-      Some(v) => v,
+  fn value(&mut self, arg: A) -> u32 {
+    match self.value.get(&arg) {
+      Some(v) => *v,
       None => {
-        let v = (self.calculation)(arg);
-        self.value = Some(v);
+        let v = (self.calculation)(arg.clone());
+        self.value.insert(arg, v);
         v
       }
     }
@@ -34,9 +42,19 @@ fn main() {
   let simulated_user_specified_value = 10;
   let simulated_random_number = 7;
 
+  let another_user_specified_value = 20;
+  let another_simulated_random_number = 4;
+
   generate_workout_plan(
     simulated_user_specified_value,
     simulated_random_number
+  );
+
+  println!("\n======================\n");
+
+  generate_workout_plan(
+    another_user_specified_value,
+    another_simulated_random_number
   );
 }
 
