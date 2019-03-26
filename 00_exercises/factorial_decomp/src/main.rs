ca7db22649bb6d866cbbb76b0b9570c81874281c
Diego Braga <dsbrgg@gmail.com>
2019-03-26 17:14:05 +0000

    exercise: still working on factorial decomp

--- 00_exercises/factorial_decomp/src/main.rs
+++ 00_exercises/factorial_decomp/src/main.rs
@@ -3,7 +3,30 @@ fn main() {
 }
 
 fn decomp(n: i32) -> String {
+  use std::collections::HashMap;
+
+  let mut stop = false;
+  let mut exp: HashMap<&i32, &i32> = HashMap::new();
   let mut factorial = (2..=n).fold(1, |acc, num| acc * num);
   
+  let mut i = 2;
+
+  while !stop {
+    while factorial % i == 0 {
+      factorial = factorial / i;
+
+      let value = exp.get(&i);
+      if value != None { 
+        exp.insert(&i, &(*value.unwrap() + 1)); 
+      } else {
+        exp.insert(&i, &i);
+      }
+    }
+
+    println!("{:#?}", exp);
+
+    stop = true;
+  }
+
   String::new()
 }
