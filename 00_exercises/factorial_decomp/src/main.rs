02664c90e1824a037c71c71f84c9726c63d14c7a
Diego Braga <dsbrgg@gmail.com>
2019-04-14 00:31:21 +0100

    exercise: :shrug:

--- 00_exercises/factorial_decomp/src/main.rs
+++ 00_exercises/factorial_decomp/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
+  // decomp(5); // "2^3 * 3 * 5"
+  // decomp(14); // "2^11 * 3^5 * 5^2 * 7^2 * 11 * 13"
+  // decomp(17); //"2^15 * 3^6 * 5^3 * 7^2 * 11 * 13 * 17"
   // decomp(12); // "2^10 * 3^5 * 5^2 * 7 * 11"
+  // decomp(20);
   decomp(22); // "2^19 * 3^9 * 5^4 * 7^3 * 11^2 * 13 * 17 * 19"
+  // decomp(25);
 }
 
 fn factor(n: usize) -> u64 {
@@ -30,26 +35,25 @@ fn factor(n: usize) -> u64 {
 fn decomp(n: i32) -> String {
   use std::collections::HashMap;
 
+  let mut i = 2;
   let mut primes: Vec<usize> = Vec::new();
   let mut exp: HashMap<usize, i32> = HashMap::new();
   // numbers will overflow when doing the factorization, so it needs wrapping
   // https://users.rust-lang.org/t/panicked-at-arithmetic-operation-overflowed/4290
   let mut factorial: usize = (2..=n).fold(1, |acc, num| acc.wrapping_mul(num as usize));
 
-  let mut i = 2;
-
   loop {
+    if factorial / i == 0 { break; }
+
     if factorial % i == 0 {
+      // println!("i -> {}\ni x {:?}\nfactorial -> {}\nmodulo -> {}\ndivision -> {}\n", i, exp.get(&i), factorial, factorial % i, factorial / i);
       factorial = factorial / i;
-      println!("i -> {}\ni x {:?}\nfactorial -> {}\nmodulo -> {}\n", i, exp.get(&i), factorial, factorial % i);
       let value = exp.get(&i);
 
       match value {
-        None => { exp.insert(i, 1);  primes.push(i); () },
+        None => { exp.insert(i, 1); primes.push(i); () },
         Some(v) => { exp.insert(i, *v + 1); () },
       };
-
-      if factorial / i == 0 { break; }
     } else {
       i += 1;
 
