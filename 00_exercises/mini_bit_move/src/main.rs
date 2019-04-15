0ae3175da1ca21c5e254e8566e43788535fc66f7
Diego Braga <dsbrgg@gmail.com>
2019-04-15 15:23:59 +0100

    exercise: unlocked solution for mini_bit_move

--- 00_exercises/mini_bit_move/src/main.rs
+++ 00_exercises/mini_bit_move/src/main.rs
@@ -1,38 +1,24 @@
 fn main() {
-  interpreter("10", "1010101"); // "0101010"
-  interpreter("100", "1111111111"); //"0101010101"
+  // interpreter("10", "1010101"); // "0101010"
+  // interpreter("100", "1111111111"); //"0101010101"
+  interpreter("110", "1010");
 }
 
-fn interpreter(cmd: &str, bits: &str) -> String {
-  let mut index = 1;
-  let mut result = String::new();
-  let mut verifier = cmd.chars().peekable();
-  let mut instructions = cmd.chars().cycle();
+// I didn't do this, it was the best answer
+fn interpreter(tape: &str, data: &str) -> String {
+  let mut instructions = tape.chars().cycle();
+  let mut bits: Vec<char> = data.chars().collect();
   
-  loop {
-    verifier.next();
-
-    let instruction = instructions.next();
-    println!("{:?} - {}", instruction, index);
-    match instruction {
-      Some('0') => {
-        index += 1;
-
-        if verifier.peek() == Some(&&'0') { result.push('0'); }
-      },
-      Some('1') => {
-        if index > bits.len() { break; }
-
-        if bits[index-1..index] == "0"[..] {
-          result.push('1');
+  bits.into_iter()
+    .map(|mut c| {
+      while instructions.next().unwrap() == '1' {
+        if c == '0' {
+          c = '1'
         } else {
-          result.push('0');
+          c = '0'
         }
-      },
-      _ => ()
-    }
-  }
-  
-  println!("{}", result);
-  result
+      }
+
+      c
+    }).collect()
 }
