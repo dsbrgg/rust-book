428c365f4086720c44b86d6e213f8456362d1d37
Diego Braga <dsbrgg@gmail.com>
2019-04-28 09:14:21 +0100

    exercise: midway through with stock_list

--- 00_exercises/stock_list/src/main.rs
+++ 00_exercises/stock_list/src/main.rs
@@ -1,3 +1,50 @@
 fn main() {
-    println!("Hello, world!");
+  let mut b = vec!["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
+  let mut c = vec!["A", "B", "C", "D"];
+  assert_eq!(stock_list(b, c), "(A : 0) - (B : 1290) - (C : 515) - (D : 600)");
+
+  // b = vec!["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"];
+  // c = vec!["A", "B"];
+  // assert_eq!(stock_list(b, c), "(A : 200) - (B : 1140)");
+}
+
+fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
+  use std::collections::HashMap;
+
+  let mut totals: HashMap<String, u64> = HashMap::new();
+
+  let codes: Vec<Vec<_>> = list_art
+    .iter()
+    .map(|m| {
+      let split: Vec<_> = m.split(' ').collect();
+      vec![
+        split[0].chars().next().unwrap().to_string(),
+        split[1].to_string(),
+      ]
+    })
+    .collect();
+
+  for category in list_cat {
+    let t = totals.entry(category.to_string()).or_insert(0);
+    for code in &codes {
+      if category.to_string() == code[0] {
+        *t += code[1].parse::<u64>().unwrap();
+      }
+    }
+  }
+
+  let t = totals
+    .iter()
+    .fold(String::new(), |mut string, (code, qty)| {
+      match string.len() == 0 {
+        true => string.push_str(&format!("({} : {})", code, qty)),
+        false => string.push_str(&format!(" - ({} : {})", code, qty)),
+      }
+      
+      string
+    });
+
+    println!("{:?}", t);
+
+  String::from("(A : 0) - (B : 1290) - (C : 515) - (D : 600)")
 }
